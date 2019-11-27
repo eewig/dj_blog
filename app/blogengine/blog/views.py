7be426ebee0d17e6f44cc1b5d2760b8b2dbe9dad
LilPumpIsGod <alosha.alosh@ukr.net>
2019-11-27 22:37:39 +0200

    added tag creating

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.shortcuts import redirect
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .forms import TagForm, PostForm
 
 
 # Create your views here.
 def posts_list(request):
 	posts = Post.objects.all()
 	return render(request, 'blog/index.html', context={'posts': posts})
+
+class PostCreate(View):
+	def get(self, request):
+		form = PostForm()
+		return render(request, 'blog/post_create_form.html', context={'form': form})
 
 
 class PostDetail(ObjectDetailMixin, View):
@@ -22,12 +28,21 @@ def tags_list(request):
 	return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-class TagDetail(ObjectDetailMixin, View):
-	model = Tag 
-	template = 'blog/tag_detail.html'
-
-
 class TagCreate(View):
 	def get(self, request):
 		form = TagForm()
 		return render(request, 'blog/tag_create.html', context={'form': form})
+
+	def post(self, request):
+		bound_form = TagForm(request.POST)
+
+		if bound_form.is_valid():
+			new_tag = bound_form.save()
+			return redirect(new_tag)
+		return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
+
+class TagDetail(ObjectDetailMixin, View):
+	model = Tag 
+	template = 'blog/tag_detail.html'
+
