9b7c06c245bfebb802743284da1824b6309f89ce
LilPumpIsGod <alosha.alosh@ukr.net>
2019-12-03 23:47:13 +0200

    added deletion posts n tags

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import View
-
+from django.urls import reverse
 from .models import Post, Tag
 from .utils import *
 from .forms import TagForm, PostForm
@@ -27,6 +27,12 @@ class PostUpdate(ObjectUpdateMixin, View):
 	template = 'blog/post_update_form.html'
 
 
+class PostDelete(ObjectDeleteMixin, View):
+	model = Post
+	template = 'blog/obj_delete_form.html'
+	redirect_url = 'posts_list_url'
+
+
 def tags_list(request):
 	tags = Tag.objects.all()
 	return render(request, 'blog/tags_list.html', context={'tags': tags})
@@ -47,17 +53,10 @@ class TagUpdate(ObjectUpdateMixin, View):
 	model_form = TagForm
 	template = 'blog/tag_update_form.html'
 
-	# def get(self, request, slug):
-	# 	tag = Tag.objects.get(slug__iexact=slug)
-	# 	bound_form = TagForm(instance=tag)
-	# 	return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
-	#
-	# def post(self, request, slug):
-	# 	tag = Tag.objects.get(slug__iexact=slug)
-	# 	bound_form = TagForm(request.POST, instance=tag)
-	#
-	# 	if bound_form.is_valid():
-	# 		new_tag = bound_form.save()
-	# 		return redirect(new_tag)
-	# 	return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+
+class TagDelete(ObjectDeleteMixin, View):
+	model = Tag
+	template = 'blog/obj_delete_form.html'
+	redirect_url = 'tags_list_url'
+
 
